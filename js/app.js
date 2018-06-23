3876f98ec40b401dbd9c4a139d4eb422eeb9d687
Clau1889 <clau.blanco1889@gmail.com>
2018-06-22 23:19:34 -0500

    En este commit se realiza splash screen

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    // ******************FUNCION PARA SPLASH******************//
+    setTimeout(function() {
+       $('#splash').fadeOut(500);
+    }, 1000);
+
+});
